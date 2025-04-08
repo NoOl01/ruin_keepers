9e038acf628a5b31a6cba574f1dc61c9de65c5d0
dobry <dobryjartem738@gmail.com>
2025-04-08 20:24:41 +0300

    add new life

--- KonkursProject/src/About.tsx
+++ KonkursProject/src/About.tsx
@@ -1,50 +1,23 @@
 import "./App.css"
-import logo from "./assets/logot.svg"
-import Goth from "./assets/Goth.svg"
-import rec from "./assets/rec.svg"
-import {Link} from "react-router-dom";
-import {useState} from "react";
-import SignupPopup from "./PopUp.tsx";
+import subsctract from "./assets/subsctrac.png"
+import logot from "./assets/logot.svg"
+
+
 export default function About(){
 
-    const [open, setOpen] = useState(false);
+
     return (
         <>
-       <div className="w-full flex flex-col items-center justify-center  bg-white">
-
-           <div className="w-full h-200 backImg"></div>
-
-            <div className="w-full flex flex-col items-center justify-center  relative gap-4 bg-inherit">
-                <h1 className="bg-inherit text-4xl font-bold relative bottom-50">О НАС</h1>
-                <img className="bg-inherit relative bottom-50" src={logo} alt=""/>
-                <p className="text-2xl text-center w-1/3 relative bottom-50">Это независимое волонтерское движение, которое заботится об архитектурном наследии в Калининградской области, а в особенности — о руинах кирх и замков.</p>
-
+            <div className="w-full h-full">
+                <img className="h-full" src={subsctract} alt=""/>
             </div>
 
-           <div className="w-full flex flex-col items-center justify-center  relative bottom-50 gap-4 bg-inherit">
-               <img src={Goth} alt=""/>
+            <div className="w-full bg-inherit justify-center flex">
+                <p className="bg-inherit">О нас</p>
+                <img  src={logot} alt=""/>
+            </div>
 
-               <div className={"flex justify-center items-center gap-8 "}>
-                   <div className="w-1/3 text-3xl flex flex-col gap-10">
-                       <p>
-                           В путешествии вы увидите достопримечательности «Готического кольца», познакомитесь с людьми, которые возрождают историческое наследие, услышите много захватывающих рассказов об исторических памятниках, личностях и событиях нашего края, узнаете об идее эстетики руин и работе нашего движения, и, наконец, поймете, почему мы все живем в огромном музее под открытым небом!
-                       </p>
-
-                       <p className="font-bold">9:00-19:00</p>
-                       <p className="font-bold">Дата: 9 апреля</p>
-                       <button  onClick={() => setOpen(true)} className="bg-[#FFCF3F] rounded-4xl h-20">Хочу записаться</button>
-                       {open && <SignupPopup onClose={() => setOpen(false)} />}
-                       <Link className="text-center" to="/moregothring">Подробнее</Link>
-                   </div>
-                   <div className="w-1/3">
-                       <img src={rec} alt=""/>
-                   </div>
-               </div>
-           </div>
-
-
-
-       </div>
         </>
+
     )
 }
--- KonkursProject/src/App.tsx
+++ KonkursProject/src/App.tsx
@@ -1,35 +1,34 @@
-
 import './App.css'
-import {BrowserRouter, Routes, Route, NavLink, Navigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom";
 import About from "./About.tsx";
 
 import Gallery from "./Gallery.tsx";
 import MoreGothRing from "./MoreGothRing.tsx";
 
-import AdminLogin from "./Admin/AdminLogin.tsx";
-import AdminDashboard from "./Admin/AdminDashboard.tsx";
-import AdminChangePassword from "./Admin/AdminChangePassword.tsx";
-import AdminDelete from "./Admin/AdminDelete.tsx";
-import AdminRegister from "./Admin/AdminRegister.tsx";
-import AdminCreateTour from "./Admin/AdminCreateTour.tsx";
 
 import TourDetail from "./TourDetail.tsx";
 import TourList from "./TourList.tsx";
-
-
+import logo from "./assets/logo.svg";
 
 
 function App() {
-    const token = localStorage.getItem("token")
 
+    const handleCopy = () => {
+        const phone = '+7 (952) 796-03-92';
+        navigator.clipboard.writeText(phone).then(() => {
+            alert('Номер скопирован в буфер обмена!');
+        });
+    }
 
     return (
         <BrowserRouter>
-            <div className="w-full h-20 flex justify-around items-center drop-shadow-2xl">
+            <div className="w-full h-20 flex justify-around items-center">
+
+                <img className="size-12" src={logo} alt=""/>
 
                 <NavLink
                     to="/"
-                    className={({ isActive }) =>
+                    className={({isActive}) =>
                         isActive
                             ? "text-yellow-500 text-2xl border-b-4 border-yellow-400 pb-1"
                             : "text-black text-2xl"
@@ -40,7 +39,7 @@ function App() {
 
                 <NavLink
                     to="/guides"
-                    className={({ isActive }) =>
+                    className={({isActive}) =>
                         isActive
                             ? "text-yellow-500 text-2xl border-b-4 border-yellow-400 pb-1"
                             : "text-black text-2xl"
@@ -51,7 +50,7 @@ function App() {
 
                 <NavLink
                     to="/gallery"
-                    className={({ isActive }) =>
+                    className={({isActive}) =>
                         isActive
                             ? "text-yellow-500 text-2xl border-b-4 border-yellow-400 pb-1"
                             : "text-black text-2xl"
@@ -61,32 +60,19 @@ function App() {
                 </NavLink>
 
                 <div className="w-1/2"></div>
-                <p className="text-2xl">+7 (952) 796-03-92</p>
+                <p className="text-2xl cursor-pointer" onClick={handleCopy}>
+                    +7 (952) 796-03-92
+                </p>
             </div>
 
             <Routes>
 
-                <Route path="/admin" element={<AdminLogin />} />
-                {token ? (
-                    <>
-                        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                        <Route path="/admin/register" element={<AdminRegister />} />
-                        <Route path="/admin/change-password" element={<AdminChangePassword />} />
-                        <Route path="/admin/delete" element={<AdminDelete />} />
-                        <Route path="/admin/tours/create" element={<AdminCreateTour />} />
-                        <Route path="*" element={<Navigate to="/admin/dashboard" />} />
-                    </>
-                ) : (
-                    <Route path="*" element={<Navigate to="/admin/login" />} />
-                )}
 
-
-
-                <Route path="/" element={<About />} />
-                <Route path="/guides" element={<TourList/>} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/moregothring" element={<MoreGothRing />} />
-                <Route path="/tour/:id" element={<TourDetail />} />
+                <Route path="/" element={<About/>}/>
+                <Route path="/guides" element={<TourList/>}/>
+                <Route path="/gallery" element={<Gallery/>}/>
+                <Route path="/moregothring" element={<MoreGothRing/>}/>
+                <Route path="/tour/:id" element={<TourDetail/>}/>
 
             </Routes>
         </BrowserRouter>
